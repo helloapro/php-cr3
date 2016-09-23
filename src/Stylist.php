692787882ef576cef0e6689a403ab0b457dd56dc
April Peng <helloaprilpeng@gmail.com>
2016-09-23 11:13:12 -0700

    build Stylist class with save and getAll functions

---
+++ src/Stylist.php
@@ -0,0 +1,48 @@
+<?php
+    class Stylist
+    {
+        private $name;
+        private $id;
+
+//constructor
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+//getters and setters
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stylists (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->exec("SELECT * FROM stylists";);
+            $stylists = array();
+            foreach ($returned_stylists as $stylist) {
+                $name = $stylist['name'];
+                $id = $stylist['id'];
+                $new_stylist = new Stylist($name, $id);
+                array_push($stylists, $new_stylist);
+            }
+        }
+    }
+?>
